361178208d2718ae8af7a6b02ffcd6b211b8b8c0
David <davidmeza@outlook.com>
2015-08-14 17:52:32 -0400

    add starters count scraper

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,4 +1,22 @@
 class LeaguesController < ApplicationController
+
+  def new
+    #form to submit league_id for scraper
+  end
+
+  def create
+    results = Scraper.new(league_params)
+    # league = League.new(id: results.league_id)
+  end
+
   def show
   end
+
+
+  private
+
+  def league_params
+    params.require(:league).permit(:id)
+  end
+
 end
