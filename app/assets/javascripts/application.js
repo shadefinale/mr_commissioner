87fbcc2bb20de581aa5d8a57cc09d3985850d087
David <davidmeza@outlook.com>
2015-08-14 16:24:54 -0400

    add views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+//custom select box
+
+$(function(){
+    $('select.styled').customSelect();
+});
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+
+  def login
+    render template: 'layouts/login'
+  end
+
+  def table
+  end
+end
