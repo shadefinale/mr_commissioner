d6cf77b7136008d4c3142126bc26312dbe128303
shadefinale <shadefinale@gmail.com>
2015-08-14 09:11:14 -0500

    Basic dashboard table set up with specs.

--- spec/features/leagues_spec.rb
+++ spec/features/leagues_spec.rb
@@ -7,5 +7,9 @@ feature 'Viewing Dashboard' do
   scenario 'showing the dashboard' do
     visit league_path(1)
     expect(page).to have_content('League Name')
+    # John Sportsfan also expects the page to have a table on it.
+    expect(page).to have_xpath("//table/tr/th[.='Statistic']")
+    # John Sportsfan expects to see a row with column 'Best Player by Points.'
+    expect(page).to have_xpath("//table/tr/td[.='Best Player by Points']")
   end
 end
--- spec/views/leagues/show_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe "leagues/show.html.erb" do
-
-  it "should show 'league name' on the page" do
-    render
-    expect(rendered).to have_content("League Name")
-  end
-end
